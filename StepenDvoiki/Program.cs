4e7e43b84c6705027cc1fad8e26da4ead51dff5c
anananass <ananauze@gmail.com>
2023-05-09 13:56:16 +0400

    Add project files.

---
+++ StepenDvoiki/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepenDvoiki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            int basicNumber = 2;
+            int degree = 0;
+            int result = 1;
+            int minNumberValue = 2;
+            int maxNumberValue = 512;
+            Random random = new Random();
+            int finalNumber = random.Next(minNumberValue, maxNumberValue + 1);
+
+            while (result <= finalNumber) 
+            {
+                degree++;
+                result *= basicNumber;
+            }  
+
+            Console.WriteLine($"Случайное число: {finalNumber}");
+            Console.WriteLine($"Нужная степень числа {basicNumber} - {degree}");
+            Console.WriteLine($"Число {basicNumber} в найденной степени - {result}");
+            Console.ReadLine();
+        }
+    }
+}
